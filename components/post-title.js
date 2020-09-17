952b5c5d5162b44f7484b870a96d62193041320b
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-09-17 17:05:00 +0200

    post-title

--- components/post-title.js
+++ components/post-title.js
@@ -1,13 +1,7 @@
-import animations from "../styles/animations.module.scss";
-
 export default function PostTitle({ children }) {
   return (
-    <div
-      className={`border-b-4 ${animations.hueRotate} ${animations.bottomShadow}`}
-    >
-      <h1 className="text-left text-5xl md:text-6xl lg:text-7xl font-bold tracking-tighter leading-tight md:leading-none mb-12 md:text-left">
-        {children}
-      </h1>
-    </div>
+    <h1 className="text-left text-5xl md:text-6xl lg:text-7xl font-bold tracking-tighter leading-tight md:leading-none mb-12 md:text-left">
+      {children}
+    </h1>
   );
 }
