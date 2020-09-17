1a3dd41a72d2f04ea161e307a885c72349dba654
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-09-17 11:09:28 +0200

    mobile container width full

--- components/container.js
+++ components/container.js
@@ -1,3 +1,5 @@
 export default function Container({ children }) {
-  return <div className="container w-3/4 mx-auto px-5">{children}</div>;
+  return (
+    <div className="container w-3/4 sm: w-full mx-auto px-5">{children}</div>
+  );
 }
